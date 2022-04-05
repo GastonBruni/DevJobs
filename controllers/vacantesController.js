02e10a11f836cc87ad08f8c334957fb6f428d1f1
Gaston <Gastonbrunijavier@gmail.com>
2022-04-04 22:27:35 -0300

    Mostramos vacantes mediante vacantesController..

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -12,12 +12,26 @@ exports.formularioNuevaVacante = (req, res) => {
 exports.agregarVacante = async (req, res) => {
     const vacante = new Vacante(req.body);
 
-    //crear arreglo de habilidades (skills)
-    vacante.skills = req.body.skills.split(',');
+   // crear arreglo de habilidades (skills)
+   vacante.skills = req.body.skills.split(',');
 
     //almacenarlo en la base de datos
     const nuevaVacante = await vacante.save();
 
     //redireccionar
     res.redirect(`/vacantes/${nuevaVacante.url}`);
+} 
+
+// muestra una vacante
+exports.mostrarVacante = async (req, res, next) => {
+    const vacante = await Vacante.findOne({ url: req.params.url });
+    
+    // si no hay resultados
+    if(!vacante) return next();
+
+    res.render('vacante', {
+        vacante,
+        nombrePagina : vacante.titulo,
+        barra: true
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 const vacantesController = require('../controllers/vacantesController');
 
-
 module.exports = () => {
     router.get('/', homeController.mostrarTrabajos)
 
     // crear vacantes
     router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante);
     router.post('/vacantes/nueva',vacantesController.agregarVacante);
+    
+    // Mostrar Vacante (singular)
+    router.get('/vacantes/:url', vacantesController.mostrarVacante);
     return router;
 }
